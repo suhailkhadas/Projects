35354eb1235011c46a2c4dfbf0126e2c3e7c8b22
suhailkhadas <77909218+suhailkhadas@users.noreply.github.com>
2025-09-12 20:01:11 +0530

    Add files via upload

---
+++ Module2/Task 2.py
@@ -0,0 +1,7 @@
+
+x=0
+for i in range(1,51):
+    x = x + i
+print(f"Toatl Sum 0f 0-50 is:  {x}" )    
+
+
--- Module2/Task1.py
+++ Module2/Task1.py
@@ -1,1 +1,5 @@
-
+a=int(input("Enter Number: "))
+if a%2 == 0:
+    print(str(a) + " Number is Even")
+else:
+    print(str(a) + " Number is Odd")
